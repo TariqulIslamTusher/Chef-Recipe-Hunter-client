a0e40c1a33975b2abdaaa77b38506a0befb06f75
TariqulIslamTusher <islamtariqul652@gmail.com>
2023-05-05 17:35:47 +0600

    banner part redesigned

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -15,7 +15,7 @@ const Home = () => {
             
             <Banner></Banner>
             <div className='bg-gradient-to-b from-gray-900 via-gray-300 to-cyan-700 border-t-indigo-300'>
-                <div className='grid grid-cols-1 px-4 md:grid-cols-3 container mx-auto gap-8 -translate-y-40'>
+                <div className='grid grid-cols-1 px-4 md:grid-cols-3 container mx-auto gap-8 md:-translate-y-40'>
                     {
                         chefsInfo.map(singleInfo => <ChefCard key={singleInfo.id} singleInfo={singleInfo}></ChefCard>)
                     }
